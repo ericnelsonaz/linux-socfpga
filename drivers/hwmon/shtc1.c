d9d5cd0f74c7bccba3fa84996d0886966d8d3ea8
Eric Nelson <eric@nelint.com>
2023-09-08 16:45:38 -0700

    hwmon: shtc1: backport support for device tree bindings
    See https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/commit/drivers/hwmon/shtc1.c?id=be7373b60df5020c00dfac4db185a6b4e9f98395
    
    Signed-off-by: Eric Nelson <eric@nelint.com>

--- drivers/hwmon/shtc1.c
+++ drivers/hwmon/shtc1.c
@@ -23,6 +23,7 @@
 #include <linux/hwmon-sysfs.h>
 #include <linux/err.h>
 #include <linux/delay.h>
+#include <linux/of.h>
 #include <linux/platform_data/shtc1.h>
 
 /* commands (high precision mode) */
@@ -186,6 +187,7 @@ static int shtc1_probe(struct i2c_client *client,
 	struct device *hwmon_dev;
 	struct i2c_adapter *adap = client->adapter;
 	struct device *dev = &client->dev;
+	struct device_node *np = dev->of_node;
 
 	if (!i2c_check_functionality(adap, I2C_FUNC_I2C)) {
 		dev_err(dev, "plain i2c transactions not supported\n");
@@ -215,8 +217,14 @@ static int shtc1_probe(struct i2c_client *client,
 	data->setup.high_precision = true;
 	data->client = client;
 
-	if (client->dev.platform_data)
-		data->setup = *(struct shtc1_platform_data *)dev->platform_data;
+	if (np) {
+		data->setup.blocking_io = of_property_read_bool(np, "sensirion,blocking-io");
+		data->setup.high_precision = !of_property_read_bool(np, "sensicon,low-precision");
+	} else {
+		if (client->dev.platform_data)
+			data->setup = *(struct shtc1_platform_data *)dev->platform_data;
+	}
+
 	shtc1_select_command(data);
 	mutex_init(&data->update_lock);
 
@@ -238,8 +246,19 @@ static const struct i2c_device_id shtc1_id[] = {
 };
 MODULE_DEVICE_TABLE(i2c, shtc1_id);
 
+static const struct of_device_id shtc1_of_match[] = {
+	{ .compatible = "sensirion,shtc1" },
+	{ .compatible = "sensirion,shtw1" },
+	{ .compatible = "sensirion,shtc3" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, shtc1_of_match);
+
 static struct i2c_driver shtc1_i2c_driver = {
-	.driver.name  = "shtc1",
+	.driver = {
+		.name = "shtc1",
+		.of_match_table = shtc1_of_match,
+	},
 	.probe        = shtc1_probe,
 	.id_table     = shtc1_id,
 };
